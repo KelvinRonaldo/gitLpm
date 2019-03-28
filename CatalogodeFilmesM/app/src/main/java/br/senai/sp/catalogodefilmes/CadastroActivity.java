51ed1f38f3530b76630ea8e9802d43b3e3d545b5
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2019-03-28 16:57:33 -0300

    Foto vinda da camera

--- CatalogodeFilmesM/app/src/main/java/br/senai/sp/catalogodefilmes/CadastroActivity.java
+++ CatalogodeFilmesM/app/src/main/java/br/senai/sp/catalogodefilmes/CadastroActivity.java
@@ -3,7 +3,10 @@ package br.senai.sp.catalogodefilmes;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,17 +19,23 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.URI;
 
 import br.senai.sp.dao.FilmeDAO;
 import br.senai.sp.modelo.Filme;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    public static final int GALERIA_REQUEST = 1;
+    public static final int CAMERA_REQUEST = 2;
     private CadastroFilmeHelper helper;
     private ImageButton btnCamera;
     private ImageButton btnGaleria;
     private ImageView imgFilme;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +50,40 @@ public class CadastroActivity extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent
+//                INTENT DE ABRIR A CAMERA
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                                                            ↑ação de abrir a camera
+//                DANDO NOME AO ARQUIVO GERADO PELO INTENT
+                String nomeArquivo = "/IMG_" + System.currentTimeMillis() + ".jpg";
+                caminhoFoto = getExternalFilesDir(null) + nomeArquivo;
+                Log.d("TEMPO", nomeArquivo);
+                File fileFoto = new File(caminhoFoto);
+
+
+                Uri fotoUri = FileProvider.getUriForFile(
+                        CadastroActivity.this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        fileFoto
+                );
+//                PENDURANDO NA INTENT UMA SAIDA, QUE É O ARQUIVO GERADO PELA CAMERA
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+
+                startActivityForResult(intentCamera, CAMERA_REQUEST);
             }
         });
 
         btnGaleria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                INTENT DE ABRIR A GALERIA
                 Intent intentGaleria = new Intent(Intent.ACTION_GET_CONTENT);
+//                                                            ↑ação de pegar conteudo
+//                DIZENDO QUAL O TIPO DE ARQUIVO QUE VIRÁ DA INTENT
                 intentGaleria.setType("image/*");
-                startActivityForResult(intentGaleria, 1);
+//                                        ↑tipo do arquivo que será buscado
+//                MANDANDO O RESULTADO DA REQUISIÇÃO PARA O MÉTODO onActivityResult()
+                startActivityForResult(intentGaleria, GALERIA_REQUEST);
+//                                        ↑intent            ↑codigo de requisição
             }
         });
 
@@ -74,16 +107,25 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        InputStream inputStream = null;
-        try {
-            if(resultCode != 0){
-                Log.d("RETORNO", String.valueOf(resultCode));
-                inputStream = getContentResolver().openInputStream(data.getData());
-                Bitmap bitmapFactory = BitmapFactory.decodeStream(inputStream);
-                imgFilme.setImageBitmap(bitmapFactory);
+        if(resultCode == RESULT_OK){
+            try {
+                switch (requestCode){
+                    case GALERIA_REQUEST:
+                        Log.d("GALERIA", String.valueOf(resultCode));
+                        InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                        Bitmap bitmapGaleria = BitmapFactory.decodeStream(inputStream);
+                        Bitmap bmGaleriaReduzido = Bitmap.createScaledBitmap(bitmapGaleria, 300, 300, true);
+                        imgFilme.setImageBitmap(bmGaleriaReduzido);
+                        break;
+                    case CAMERA_REQUEST:
+                        Bitmap bitmapCamera = BitmapFactory.decodeFile(caminhoFoto);
+                        Bitmap bmCameraReduzido = Bitmap.createScaledBitmap(bitmapCamera, 300, 300, true);
+                        imgFilme.setImageBitmap(bmCameraReduzido);
+                        break;
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
 
     }
